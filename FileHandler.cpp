c95e6dc757993291f8371fee788c37ea161d1f69
adamromero <adamromero89@gmail.com>
2013-07-26 21:01:14 -0700

    Update FileHandler.cpp

--- FileHandler.cpp
+++ FileHandler.cpp
@@ -2,36 +2,30 @@
 #include "Banking.h"
 
 FileHandler::FileHandler(std::string name){
-  	filename = name.c_str();
-	inputFile = new std::ifstream(filename);
-	outputFile = NULL;
-}
-
-FileHandler::~FileHandler(){
-	delete inputFile;
-	if(outputFile)
-		delete outputFile;
+	filename = name.c_str();
+	inputFile.open(filename);
 }
 
 bool FileHandler::exists(){ 
-	return (*inputFile);
+	return (inputFile);
 }
 
 void FileHandler::readFromFile(std::string *line){
 	int idx = 0;
-	while(inputFile->good()){
-		getline(*inputFile, line[idx++]);
+	while(inputFile.good()){
+		getline(inputFile, line[idx++]);
 	}
 }
 
 void FileHandler::writeToFile(std::string name, Account &save, Account &check){
-	outputFile = new std::ofstream(filename);
-	*outputFile << name << std::endl;
-	*outputFile << save.getBalance() << std::endl;
-	*outputFile << check.getBalance() << std::endl;
-	outputFile->close();
+	outputFile.open(filename);
+	outputFile << name << std::endl;
+	outputFile << save.getBalance() << std::endl;
+	outputFile << check.getBalance() << std::endl;
+	outputFile.close();
 }
 
 void FileHandler::closeInputFile(){
-	inputFile->close();
+	inputFile.close();
 }
+
