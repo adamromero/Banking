48e08fc01b6b8e308b2058c28b07209a4f617ce9
adamromero <adamromero89@gmail.com>
2013-07-26 18:38:59 -0700

    Create BankingDriver.cpp

---
+++ BankingDriver.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "Banking.h"
+#include "FileHandler.h"
+
+int main(int argc, char *argv[]){
+  if(argc != 2){
+		std::cout << "Usage: ./bank [databasefilename]\n";
+		return 0;
+	}
+	
+	Banking program(argv[1]);
+	program.run();
+	return 0;
+}
