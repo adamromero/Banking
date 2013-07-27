8521e846a8723a6220b9d13b266654bd3b924776
adamromero <adamromero89@gmail.com>
2013-07-26 18:36:34 -0700

    Create Account.cpp

---
+++ Account.cpp
@@ -0,0 +1,24 @@
+#include "Account.h"
+  
+Account::Account(std::string strType, double dBalance) 
+	: accountType(strType), balance(dBalance){}
+	
+void Account::setBalance(double dBalance){ 
+	balance = dBalance;
+}
+
+std::string Account::getAccountType() const { 
+	return accountType;
+}
+
+double Account::getBalance() const{ 
+	return balance;
+} 
+
+void Account::makeWithdraw(double withdraw) {
+	balance -= withdraw;
+}
+
+void Account::makeDeposit(double deposit) {
+	balance += deposit;
+}
