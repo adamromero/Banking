8fa0d5d1a9c87784300a87ee13056f01f3b5a68f
adamromero <adamromero89@gmail.com>
2013-07-26 18:37:28 -0700

    Create Banking.h

---
+++ Banking.h
@@ -0,0 +1,29 @@
+#ifndef BANKING_H
+#define BANKING_H
+
+#include "Account.h"
+#include "FileHandler.h"
+
+class Banking {
+  private:
+		std::string accountHolder;
+		Account savings;
+		Account checking;
+		std::string filename;
+		bool isDeposit;
+	public:
+		Banking(std::string file);
+		void setBankingFields(std::string name, Account save, Account check);
+		std::string getAccountHolder();
+		Account getSavings();
+		Account getChecking();
+		bool manageAccount();
+		void chooseAccount();
+		void savingsTransaction();
+		void checkingTransaction();
+		void showAccountInfo();
+		void writeToFile();
+		void run();
+};
+
+#endif
